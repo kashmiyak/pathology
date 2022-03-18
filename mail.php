0cd355aacf27d66d7132898b15c1d0ffc5e0ccc0
kashmiyak <101542857+kashmiyak@users.noreply.github.com>
2022-03-18 09:21:21 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,18 @@
+
+    <?php
+//get data from form  
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email= $_POST['email'];
+$message= $_POST['Message'];
+$to = "kashmiyak@gmail.com";
+$subject = "Mail From Chandralekha Website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message ."\r\n Mobile No=" . $phone;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html")
+?>
